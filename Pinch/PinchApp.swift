bc93d787f2e89485fc6f19d7c65639a95d8723db
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2023-04-09 09:01:01 +0600

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Rehnuma Reza Deepty on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
